102452f1d8fdc51b5b804960193b49d9340518d3
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2021-07-30 12:02:35 +0530

    Bolt12, haven't set up the .t.js file, checkout index.js for the code

---
+++ bolt12/index.d.ts
@@ -0,0 +1,8 @@
+export declare type decodedinfo={
+    description:string
+    node_id:string
+}
+
+
+
+export declare function decode(paymentRequest: string, network?: Network): PaymentRequestObject;
