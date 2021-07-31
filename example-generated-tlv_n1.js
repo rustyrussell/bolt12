6cf37b1ca9d52e33d2e3bec2b8dc7d6d306f99d6
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2021-07-31 07:29:33 +0530

    added to_wire and from_wire for tu64

--- example-generated-tlv_n1.js
+++ example-generated-tlv_n1.js
@@ -15,31 +15,53 @@
 //    1. type: 254 (`tlv4`)
 //    2. data:
 //      * [`u16`:`cltv_delta`]
-
-function towire_n1_tlv3(value): Buffer
+// function toBufferBE(num, width){
+//     if (process.browser || converter === undefined) {
+//       const hex = num.toString(16);
+//       return Buffer.from(hex.padStart(width * 2, '0').slice(0, width * 2), 'hex');
+//     }
+//     return converter.fromBigInt(num, Buffer.allocUnsafe(width), true);
+// }
+function towire_tu64(value)
 {
-    _n = 0
-    Buffer buf
-    buf = Buffer.concat([buf, towire_point(value[_n++])])
-    buf = Buffer.concat([buf, towire_u64(value[_n++])])
-    buf = Buffer.concat([buf, towire_u64(value[_n++])])
-    assert(value.length() == _n)
-
-    return buf
+    var res=Buffer.alloc(8)
+    res.fill(0)
+    res.writeUInt32BE(value >> 8, 0); //write the high order bits (shifted over)
+    res.writeUInt32BE(value & 0x00ff, 4); //write the low order bits
+    return res
 }
 
-function fromwire_n1_tlv3(buffer): []
+function fromwire_tu64(buffer)
 {
-    value = []
-    _n = 0
-    value.push(fromwire_point(buffer))
-    value.push(fromwire_u64(buffer))
-    value.push(fromwire_u64(buffer))
+    var bufInt = (buf.readUInt32BE(0) << 8) + buf.readUInt32BE(4);
+    console.log(bufInt);
 }
+
+console.log(towire_tu64(1000000000))
+// function towire_n1_tlv3(value): Buffer
+// {
+//     _n = 0
+//     // Buffer buf
+//     buf = Buffer.concat([buf, towire_point(value[_n++])])
+//     buf = Buffer.concat([buf, towire_u64(value[_n++])])
+//     buf = Buffer.concat([buf, towire_u64(value[_n++])])
+//     assert(value.length() == _n)
+
+//     return buf
+// }
+
+// function fromwire_n1_tlv3(buffer): []
+// {
+//     value = []
+//     _n = 0
+//     value.push(fromwire_point(buffer))
+//     value.push(fromwire_u64(buffer))
+//     value.push(fromwire_u64(buffer))
+// }
 
 const tlvs_n1 = {
     1: [ "tlv1", towire_tu64, fromwire_tu64 ],
-    2: [ "tlv2", towire_short_channel_id, fromwire_short_channel_id ],
-    3: [ "tlv3", towire_n1_tlv3, fromwire_n1_tlv3 ],
-    254: [ "tlv4", towire_u16, fromwire_u16 ]
+    // 2: [ "tlv2", towire_short_channel_id, fromwire_short_channel_id ],
+    // 3: [ "tlv3", towire_n1_tlv3, fromwire_n1_tlv3 ],
+    // 254: [ "tlv4", towire_u16, fromwire_u16 ]
 }
