46a62aa5369e55db6b15dbc0181a261f13f906e1
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2021-08-13 08:50:09 +0530

    Offer_id bug..

--- javascript/test_bolt12.js
+++ javascript/test_bolt12.js
@@ -4,7 +4,7 @@ file=require('../test-vectors/offers.json');
 for(let i=0;i<file.length;i++){
     dec = decode(file[i]['string'])
     console.log(dec);
-    if(dec['Offer_id']!=file[i]['Offer_id'] && dec['type']!=file[i]['type']){
+    if(dec['offer_id']!=file[i]['offer_id'] && dec['type']!=file[i]['type']){
         throw Error("Fails for test vector #"+(i+1));
     }
 }
